82375a2259bd1fa46434505d184230b77a658b44
Simplyvoda <efem.vodina@gmail.com>
2022-10-17 15:46:42 +0100

    commit

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     users_data = requests.get(url + "/users").json()
 
     with open("todo_all_employees.json", 'w') as jsonfile:
-            json.dump({
+        json.dump({
             u.get("id"): [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
